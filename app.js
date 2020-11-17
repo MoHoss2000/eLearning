378a54b8a17d95d2cb684c2a6293c77da4f2ce0f
Mohamed Hossam <mohamed.hosammohamed@student.guc.edu.eg>
2020-11-17 15:32:06 +0200

    da

--- app.js
+++ app.js
@@ -78,23 +78,23 @@ app.use(function (err, req, res, next) {
 });
 
 
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/mostafafarid.com/chain.pem', 'utf8');
 
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca
-// };
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 
-// // var httpServer = http.createServer(app);
-// var httpsServer = https.createServer(credentials, app);
+// var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-// // httpServer.listen(8080);
-// httpsServer.listen(443 || process.env.PORT);
-
-
+// httpServer.listen(8080);
+httpsServer.listen(443 || process.env.PORT);
 
 
-app.listen(process.env.PORT || 80);
+
+
+// app.listen(process.env.PORT || 80);
